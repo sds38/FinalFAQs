c8c7cd3909ba9d7c0d52d5df0e605dfdfe3d4121
shail <sds38@njit.edu>
2019-08-06 00:04:17 -0400

    home modified

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,57 +6,48 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">Questions
-                        <a class="btn btn-primary float-right" href="{{ route('questions.create') }}">
-                            Create a Question
-                        </a>
+                        <a class="btn btn-success float-right" href="{{ route('questions.create') }}">
+                            Add Question
+                        </a></div>
 
-                        <div class="card-body">
+                    <div class="card-body">
 
-                            <div class="card-deck">
-                                @forelse($questions as $question)
-                                    <div class="col-sm-4 d-flex align-items-stretch">
-                                        <div class="card mb-3 ">
-                                            <div class="card-header">
-                                                <small class="text-muted">
-                                                    Updated: {{ $question->created_at->diffForHumans() }}
-                                                    Answers: {{ $question->answers()->count() }}
+                        <div class="card-deck">
+                            @foreach($questions as $question)
+                                <div class="col-sm-4 d-flex align-items-stretch">
+                                    <div class="card mb-3 ">
+                                        <div class="card-header">
+                                            <small class="text-muted">
+                                                Updated: {{ $question->created_at->diffForHumans() }}
+                                                Answers: {{ $question->answers()->count() }}
 
-                                                </small>
-                                            </div>
-                                            <div class="card-body">
-                                                <p class="card-text">{{$question->body}}</p>
-                                            </div>
-                                            <div class="card-footer">
-                                                <p class="card-text">
+                                            </small>
+                                        </div>
+                                        <div class="card-body">
+                                            <p class="card-text">{{$question->body}}</p>
+                                        </div>
+                                        <div class="card-footer">
+                                            <p class="card-text">
 
-                                                    <a class="btn btn-primary float-right" href="{{ route('questions.show', ['id' => $question->id]) }}">
-                                                        View
-                                                    </a>
-                                                </p>
-                                            </div>
+                                                <a class="btn btn-primary float-right" href="{{ route('questions.show', ['id' => $question->id]) }}">
+                                                    View
+                                                </a>
+                                            </p>
                                         </div>
                                     </div>
-                                @empty
-
-                                    There are no questions to view, you can create a question.
-
-
-                                @endforelse
-
-
-                            </div>
-
+                                </div>
+                            @endforeach
                         </div>
-                        <div class="card-footer">
-                            <div class="float-right">
-                                {{ $questions->links() }}
-                            </div>
-                        </div>
-
 
                     </div>
+                    <div class="card-footer">
+                        <div class="float-right">
+                            {{ $questions->links() }}
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
-        @endsection 
     </div>
+@endsection
